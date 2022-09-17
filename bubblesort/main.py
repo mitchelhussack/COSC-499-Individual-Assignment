b43d9ac80518fb32e6324e30e18b67e84e79a59e
unknown <mitchelhussack@gmail.com>
2022-09-16 20:26:00 -0700

    Added test class to ensure the code runs correctly

--- bubblesort/main.py
+++ bubblesort/main.py
@@ -1,4 +1,7 @@
+
+
 def bubbleSort(arr):
+
     n = len(arr)
     for i in range(n):
         for j in range(0, n - i - 1):
@@ -6,10 +9,12 @@ def bubbleSort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     print("Bubble Sorted Array:")
+    arr2 = []
     for i in range(len(arr)):
-        print("%d" % arr[i], end=' ')
+        arr2.append(arr[i])
+    return arr2
 
 
-arr = [2, 1, 10, 23]
-
-bubbleSort(arr)
+# arr = [2, 1, 10, 23]
+#
+# print(bubbleSort(arr))
